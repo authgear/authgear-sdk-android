d795ff00188c0705478b5ac6ebfa573375caf167
Carmen <carmen@oursky.com>
2021-01-08 18:32:11 +1100

    Change AuthorizeOptions properties to var
    That would be more user friendly for user to assign optional properties

--- sdk/src/main/java/com/oursky/authgear/PromoteOptions.kt
+++ sdk/src/main/java/com/oursky/authgear/PromoteOptions.kt
@@ -4,15 +4,15 @@ data class PromoteOptions @JvmOverloads constructor(
     /**
      * Redirection URI to which the response will be sent after authorization.
      */
-    val redirectUri: String,
+    var redirectUri: String,
     /**
      * OAuth 2.0 state value.
      */
-    val state: String? = null,
+    var state: String? = null,
     /**
      * UI locale tags
      */
-    val uiLocales: List<String>? = null,
+    var uiLocales: List<String>? = null,
     /**
      * WeChat redirect uri is needed when integrating WeChat login
      * The weChatRedirectURI will be called when user click the login with WeChat button
