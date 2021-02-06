66702498d3df2cb299c5276487da466f00f8dada
Louis Chan <louischan@oursky.com>
2021-02-06 11:09:00 +0800

    Support x_page in example

--- javasample/src/main/java/com/oursky/authgeartest/MainActivity.java
+++ javasample/src/main/java/com/oursky/authgeartest/MainActivity.java
@@ -17,6 +17,7 @@ import com.oursky.authgear.UserInfo;
 public class MainActivity extends AppCompatActivity {
     private EditText mClientId;
     private EditText mEndpoint;
+    private EditText mPage;
     private CheckBox mIsThirdParty;
     private TextView mLoading;
     private View mAuthorize;
@@ -37,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
 
         mClientId = findViewById(R.id.clientIdInput);
         mEndpoint = findViewById(R.id.endpointInput);
+        mPage = findViewById(R.id.pageInput);
         mIsThirdParty = findViewById(R.id.isThirdPartyInput);
         mLoading = findViewById(R.id.loading);
         mAuthorize = findViewById(R.id.authorize);
@@ -53,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
                 mIsThirdParty.isChecked()
             )
         );
-        mAuthorize.setOnClickListener(view -> viewModel.authorize());
+        mAuthorize.setOnClickListener(view -> viewModel.authorize(mPage.getText().toString()));
         mAuthenticateAnonymously.setOnClickListener(view -> viewModel.authenticateAnonymously());
         mOpenSettings.setOnClickListener(view -> viewModel.openSettings());
         mPromoteAnonymousUser.setOnClickListener(view -> viewModel.promoteAnonymousUser());
@@ -62,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
 
         mClientId.setText(viewModel.clientID().getValue());
         mEndpoint.setText(viewModel.endpoint().getValue());
+        mPage.setText(viewModel.page().getValue());
         mIsThirdParty.setChecked(viewModel.isThirdParty().getValue());
 
         viewModel.isConfigured().observe(this, isConfigured -> {
