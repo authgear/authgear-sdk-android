998f73ba27fdad3ceba886013a3d804aba19426c
Carmen <carmen@oursky.com>
2021-07-30 16:21:04 +1000

    Add close button to WebViewActivity and add authgear theme

--- sdk/src/main/java/com/oursky/authgear/WebViewActivity.kt
+++ sdk/src/main/java/com/oursky/authgear/WebViewActivity.kt
@@ -1,10 +1,13 @@
 package com.oursky.authgear
 
+import android.R
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
@@ -12,6 +15,8 @@ import androidx.appcompat.app.AppCompatActivity
 
 internal class WebViewActivity : AppCompatActivity() {
     companion object {
+        private const val MENU_ID_CANCEL = 1
+
         fun createIntent(context: Context, uri: Uri): Intent {
             val intent = Intent(context, WebViewActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
@@ -51,6 +56,24 @@ internal class WebViewActivity : AppCompatActivity() {
             webView.goBack()
         } else {
             super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menu?.add(Menu.NONE, MENU_ID_CANCEL, Menu.NONE, R.string.cancel)
+            ?.setIcon(R.drawable.ic_menu_close_clear_cancel)
+            ?.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        MENU_ID_CANCEL -> {
+            finish()
+            true
+        }
+
+        else -> {
+            super.onOptionsItemSelected(item)
         }
     }
 }
