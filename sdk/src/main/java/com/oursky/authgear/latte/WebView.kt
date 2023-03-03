744b9206fa6452ce4cba4ff448ba00c257bbd715
kiootic <kiootic@oursky.com>
2023-03-03 19:31:41 +0800

    Show soft input on initial load

--- sdk/src/main/java/com/oursky/authgear/latte/WebView.kt
+++ sdk/src/main/java/com/oursky/authgear/latte/WebView.kt
@@ -2,6 +2,9 @@ package com.oursky.authgear.latte
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputConnection
+import android.view.inputmethod.InputMethodManager
 import com.oursky.authgear.AuthgearException
 
 @SuppressLint("SetJavaScriptEnabled")
@@ -20,9 +23,23 @@ internal class WebView(context: Context) : android.webkit.WebView(context) {
     fun load() {
         val url = this.request?.url ?: throw AuthgearException("request not provided")
         this.loadUrl(url.toString())
+        this.requestFocus()
     }
 
     override fun setWebViewClient(client: android.webkit.WebViewClient) {
         super.setWebViewClient(WebViewClient(this, client))
+    }
+
+    override fun onCreateInputConnection(outAttrs: EditorInfo?): InputConnection? {
+        val conn = super.onCreateInputConnection(outAttrs)
+        post { showSoftInputIfNeeded() }
+        return conn
+    }
+
+    private fun showSoftInputIfNeeded() {
+        val manager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        if (manager.isAcceptingText) {
+            manager.showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
+        }
     }
 }
