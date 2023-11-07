4736b11da7f40d7e9192758ef53d380d6219faf9
Louis Chan <louischan@oursky.com>
2023-11-07 14:14:03 +0800

    Fix build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+    // Android Gradle Plugin 8.1 always use SDK Build Tools 33.0.1
+    // https://developer.android.com/build/releases/gradle-plugin#8-1-0
+    // If you ever update Android Gradle Plugin, please also update
+    // BUILD_TOOLS_VERSION in ./.github/workflows/ci.yaml
     id("com.android.library") version "8.1.2" apply false
     id("com.android.application") version "8.1.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
