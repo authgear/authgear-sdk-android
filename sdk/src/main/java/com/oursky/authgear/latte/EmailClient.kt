5fff46bfecd93f2b7ce071038c45b81508fdd15b
Louis Chan <louischan@oursky.com>
2024-07-09 16:22:43 +0800

    Query mailto intents handler to construct email client chooser

--- sdk/src/main/java/com/oursky/authgear/latte/EmailClient.kt
+++ sdk/src/main/java/com/oursky/authgear/latte/EmailClient.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.pm.LabeledIntent
 import android.content.pm.PackageManager
+import android.net.Uri
 
 data class EmailClient(val packageName: String) {
     companion object {
@@ -20,13 +21,22 @@ data class EmailClient(val packageName: String) {
         ): Intent? {
             val launchIntents: MutableList<Intent> = mutableListOf()
             val pm: PackageManager = ctx.packageManager
+            val mailtoIntent = Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:"))
+            val packageInfos = pm.queryIntentActivities(mailtoIntent, PackageManager.GET_RESOLVED_FILTER)
+            val packageNames = hashSetOf<String>()
+            for (info in packageInfos) {
+                packageNames.add(info.activityInfo.packageName)
+            }
             for (client in clients) {
-                val intent = pm.getLaunchIntentForPackage(client.packageName) ?: continue
+                packageNames.add(client.packageName)
+            }
+            for (packageName in packageNames) {
+                val intent = pm.getLaunchIntentForPackage(packageName) ?: continue
                 val info = pm.resolveActivity(intent, 0) ?: continue
                 launchIntents.add(
                     LabeledIntent(
                         intent,
-                        client.packageName,
+                        packageName,
                         info.loadLabel(pm),
                         info.icon
                     )
