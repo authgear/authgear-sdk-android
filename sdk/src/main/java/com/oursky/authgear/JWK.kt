be608469adca07faf3e5a37fa673fd08e81e1b93
Tung Wu <tung@oursky.com>
2024-07-16 23:51:26 +0800

    Fix jwk included extra 0 bytes in n and e

--- sdk/src/main/java/com/oursky/authgear/JWK.kt
+++ sdk/src/main/java/com/oursky/authgear/JWK.kt
@@ -5,11 +5,13 @@ import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.JsonPrimitive
+import java.math.BigInteger
 import java.security.MessageDigest
 import java.security.PublicKey
 import java.security.interfaces.RSAPublicKey
 import kotlin.io.encoding.Base64
 import kotlin.io.encoding.ExperimentalEncodingApi
+import kotlin.math.ceil
 
 internal data class JWK(
     val kid: String,
@@ -51,11 +53,22 @@ internal fun JWK.toSHA256Thumbprint(): String {
     return Base64.UrlSafe.encode(hashBytes).removeSuffix("=")
 }
 
+internal fun BigInteger.toUnsignedByteArray(): ByteArray {
+    // BigInteger always include a bit to represent the sign
+    // So the array length is ceil((this.bitLength() + 1)/8)
+    // This sign bit causes an extra byte to be added to the ByteArray when bitLength is just divisible by 8
+    // We want to exclude that extra byte in some cases, such as sending the bytes in a JWK as Base64urlUInt
+    val expectedLength = ceil(this.bitLength() / 8.0).toInt()
+    val bytes = this.toByteArray()
+    val startIdx = bytes.size - expectedLength
+    return bytes.sliceArray(IntRange(startIdx, bytes.size - 1))
+}
+
 internal fun publicKeyToJWK(kid: String, publicKey: PublicKey): JWK {
     val rsaPublicKey = publicKey as RSAPublicKey
     return JWK(
         kid = kid,
-        n = base64UrlEncode(rsaPublicKey.modulus.toByteArray()),
-        e = base64UrlEncode(rsaPublicKey.publicExponent.toByteArray())
+        n = base64UrlEncode(rsaPublicKey.modulus.toUnsignedByteArray()),
+        e = base64UrlEncode(rsaPublicKey.publicExponent.toUnsignedByteArray())
     )
 }
