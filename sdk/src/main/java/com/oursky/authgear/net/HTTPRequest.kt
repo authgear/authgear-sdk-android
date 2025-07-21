e60f59f0ea7bf3be1a64d29c90973ac85d11c873
Louis Chan <louischan@oursky.com>
2025-07-21 11:59:40 +0800

    Hide non-essential fields in HTTPRequest and HTTPResponse
    Giving access to the body is not very useful, unless we allow
    replacing the whole InputStream.

--- sdk/src/main/java/com/oursky/authgear/net/HTTPRequest.kt
+++ sdk/src/main/java/com/oursky/authgear/net/HTTPRequest.kt
@@ -7,6 +7,6 @@ public class HTTPRequest internal constructor(
     public val method: String,
     public val headers: MutableMap<String, MutableList<String>>,
     public val uri: URI,
-    public val body: InputStream? = null,
-    public val followRedirect: Boolean? = null
+    internal val body: InputStream? = null,
+    internal val followRedirect: Boolean? = null
 )
--- sdk/src/main/java/com/oursky/authgear/net/HTTPResponse.kt
+++ sdk/src/main/java/com/oursky/authgear/net/HTTPResponse.kt
@@ -5,5 +5,5 @@ import java.io.InputStream
 public class HTTPResponse internal constructor(
     public val statusCode: Int,
     public val headers: Map<String, List<String>>,
-    public val body: InputStream
+    internal val body: InputStream
 )
