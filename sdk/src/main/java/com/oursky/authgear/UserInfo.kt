1c309d65834af73044944c029e2938fe057bc64d
Tung Wu <tung@oursky.com>
2025-11-19 16:09:33 +0800

    Add recoveryCodeEnabled to userinfo

--- sdk/src/main/java/com/oursky/authgear/UserInfo.kt
+++ sdk/src/main/java/com/oursky/authgear/UserInfo.kt
@@ -13,6 +13,8 @@ data class UserInfo(
     val isAnonymous: Boolean,
     @SerialName("https://authgear.com/claims/user/can_reauthenticate")
     val canReauthenticate: Boolean,
+    @SerialName("https://authgear.com/claims/user/recovery_code_enabled")
+    val recoveryCodeEnabled: Boolean? = null,
     @SerialName("https://authgear.com/claims/user/roles")
     val roles: Array<String>? = null,
     @SerialName("custom_attributes")
