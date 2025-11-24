9aaf25fc20bee60d5a56248dd8cd3e45fd5fda56
Tung Wu <tung@oursky.com>
2025-11-24 15:13:07 +0800

    Ensure new authenticator type and kind won't break sdk

--- sdk/src/main/java/com/oursky/authgear/AuthenticatorKind.kt
+++ sdk/src/main/java/com/oursky/authgear/AuthenticatorKind.kt
@@ -1,12 +1,39 @@
 package com.oursky.authgear
 
-import kotlinx.serialization.SerialName
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.descriptors.PrimitiveKind
 
-@Serializable
+@Serializable(with = AuthenticatorKindSerializer::class)
 enum class AuthenticatorKind {
-    @SerialName("primary")
     PRIMARY,
-    @SerialName("secondary")
-    SECONDARY
+    SECONDARY,
+    UNKNOWN
+}
+
+object AuthenticatorKindSerializer : KSerializer<AuthenticatorKind> {
+    override val descriptor: SerialDescriptor =
+        PrimitiveSerialDescriptor("AuthenticatorKind", PrimitiveKind.STRING)
+
+    override fun deserialize(decoder: Decoder): AuthenticatorKind {
+        val value = decoder.decodeString()
+        return when (value) {
+            "primary" -> AuthenticatorKind.PRIMARY
+            "secondary" -> AuthenticatorKind.SECONDARY
+            else -> AuthenticatorKind.UNKNOWN
+        }
+    }
+
+    override fun serialize(encoder: Encoder, value: AuthenticatorKind) {
+        val stringValue = when (value) {
+            AuthenticatorKind.PRIMARY -> "primary"
+            AuthenticatorKind.SECONDARY -> "secondary"
+            AuthenticatorKind.UNKNOWN -> "unknown"
+        }
+        encoder.encodeString(stringValue)
+    }
 }
--- sdk/src/main/java/com/oursky/authgear/AuthenticatorType.kt
+++ sdk/src/main/java/com/oursky/authgear/AuthenticatorType.kt
@@ -1,16 +1,45 @@
 package com.oursky.authgear
 
-import kotlinx.serialization.SerialName
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.descriptors.PrimitiveKind
 
-@Serializable
+@Serializable(with = AuthenticatorTypeSerializer::class)
 enum class AuthenticatorType {
-    @SerialName("password")
     PASSWORD,
-    @SerialName("oob_otp_email")
     OOB_OTP_EMAIL,
-    @SerialName("oob_otp_sms")
     OOB_OTP_SMS,
-    @SerialName("totp")
-    TOTP
+    TOTP,
+    UNKNOWN
+}
+
+object AuthenticatorTypeSerializer : KSerializer<AuthenticatorType> {
+    override val descriptor: SerialDescriptor =
+        PrimitiveSerialDescriptor("AuthenticatorType", PrimitiveKind.STRING)
+
+    override fun deserialize(decoder: Decoder): AuthenticatorType {
+        val value = decoder.decodeString()
+        return when (value) {
+            "password" -> AuthenticatorType.PASSWORD
+            "oob_otp_email" -> AuthenticatorType.OOB_OTP_EMAIL
+            "oob_otp_sms" -> AuthenticatorType.OOB_OTP_SMS
+            "totp" -> AuthenticatorType.TOTP
+            else -> AuthenticatorType.UNKNOWN
+        }
+    }
+
+    override fun serialize(encoder: Encoder, value: AuthenticatorType) {
+        val stringValue = when (value) {
+            AuthenticatorType.PASSWORD -> "password"
+            AuthenticatorType.OOB_OTP_EMAIL -> "oob_otp_email"
+            AuthenticatorType.OOB_OTP_SMS -> "oob_otp_sms"
+            AuthenticatorType.TOTP -> "totp"
+            AuthenticatorType.UNKNOWN -> "unknown"
+        }
+        encoder.encodeString(stringValue)
+    }
 }
